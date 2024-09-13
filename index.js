5a57671b1fa2119378f5786d74ead491fa9422a6
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-14 00:26:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ client.on("messageCreate", message => {
    
     const channel = client.channels.cache.get('1037831299592880209'); // chat ROB
     const channel1 = client.channels.cache.get('1037831299592880209'); // chat FIN TATl3b
-    const rob = client.channels.cache.get('1047751275665698867'); //  CHAT FIN trobi
+    const rob = client.channels.cache.get('1047751275665698867'); // CHAT FIN trobi
 
     if (flag) return;
 
-
-    if (message.content.toLowerCase().replace(/\s+/g, '').startsWith('!withall') &&
+    // Remove spaces and dashes, then check the command
+    const command = message.content.toLowerCase().replace(/[-\s]+/g, '');
+    
+    if (command.startsWith('!rrstart') &&
         (message.author.id === "705434473990193182")) {
         flag = true;
 
-        
         channel.send('!rob 705434473990193182').then(() => console.log('Sent !rob command'));
         setTimeout(() => {
             channel1.send('!cf 100').then(() => console.log('Sent !bal command'));
@@ -39,9 +40,9 @@ client.on("messageCreate", message => {
             channel1.send('!dep all').then(() => console.log('Sent !dep all command'));
         }, 1200);
         setTimeout(() => {
-            rob.send('!lb ').then(() => console.log('Sent !dep all command  for rob'));
+            rob.send('!lb ').then(() => console.log('Sent !dep all command for rob'));
         }, 1500); 
     }
 }); 
-client.login(mySecret).catch(console.error); 
-                                                               
+
+client.login(mySecret).catch(console.error);
