c3334f7d1f71218dea3a8f8b6810dafa3270ed7f
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-16 00:52:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ const { Client } = require('discord.js-selfbot-v13');
 const mySecret = process.env['TOKEN']; 
 const client = new Client();
 
+// List of target user IDs
+const targetUsers = [
+    '833869005500448808', // User 1
+    '1279031333435478027', // User 2
+    '714965650291818506'  // User 3
+];
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`); 
 });
 
 client.on("messageCreate", async message => {
-    // Check if the message mentions the bot
-    if (message.mentions.has(client.user)) {
+    // Check if the message author is one of the target users
+    if (targetUsers.includes(message.author.id)) {
         try {
             // React with the custom emoji <a:11pm_huh:1037868024914522212>
             await message.react('<a:11pm_huh:1037868024914522212>');
-            console.log('Reacted with the custom emoji!');
+            console.log(`Reacted to message from ${message.author.tag}`);
         } catch (error) {
             console.error('Failed to react:', error);
         }
