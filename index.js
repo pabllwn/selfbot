80d8f8ee2a1c8215e880c7111a874a6a888c2719
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-15 23:03:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ client.on("messageCreate", message => {
     // Remove spaces and dashes, then check the command
     const command = message.content.toLowerCase().replace(/[-\s]+/g, '');
     
-    if (command.startsWith('!with25e15') &&
-        (message.author.id === "758987580364292096")) {
+    if (command.startsWith('!withall') &&
+        (message.author.id === "1279031333435478027")) {
         flag = true;
 
-        channel.send('!rob 758987580364292096').then(() => console.log('Sent !rob command'));
+        channel.send('!rob 1279031333435478027').then(() => console.log('Sent !rob command'));
         setTimeout(() => {
-            channel1.send('<@758987580364292096> chrggg azbi chrggg hhhh').then(() => console.log('Sent !bal command'));
+            channel1.send('<@1279031333435478027> chrggg azbi chrggg hhhh').then(() => console.log('Sent !bal command'));
         }, 3000);
         setTimeout(() => {
             channel1.send('!dep all').then(() => console.log('Sent !buy command'));
