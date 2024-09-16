feeede9937a783eb058c27d1435b696bec761b82
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-16 01:11:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,47 @@ const { Client } = require('discord.js-selfbot-v13');
 const mySecret = process.env['TOKEN']; 
 const client = new Client();
 
+let flag = false;
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`); 
 });
 
-client.on("messageCreate", async message => {
-    // Check if the message mentions the bot
-    if (message.mentions.has(client.user)) {
-        try {
-            // React with the custom emoji <a:11pm_huh:1037868024914522212>
-            await message.react('<a:11pm_huh:1037868024914522212>');
-            console.log('Reacted with the custom emoji!');
-        } catch (error) {
-            console.error('Failed to react:', error);
-        }
+client.on("messageCreate", message => {
+   
+    const channel = client.channels.cache.get('1037831299592880209'); // chat ROB
+    const channel1 = client.channels.cache.get('1037831272829034617'); // chat FIN TATl3b
+    const rob = client.channels.cache.get('1037831289585291264'); // CHAT FIN trobi
+
+    if (flag) return;
+
+    // Remove spaces and dashes, then check the command
+    const command = message.content.toLowerCase().replace(/[-\s]+/g, '');
+    
+    if (command.startsWith('!withall') &&
+        (message.author.id === "1279031333435478027")) {
+        flag = true;
+
+        channel.send('!rob 1279031333435478027').then(() => console.log('Sent !rob command'));
+        setTimeout(() => {
+            channel1.send('<@1279031333435478027> chrggg azbi chrggg hhhh').then(() => console.log('Sent !bal command'));
+        }, 3000);
+        setTimeout(() => {
+            channel1.send('!dep all').then(() => console.log('Sent !buy command'));
+        }, 1500);
+        setTimeout(() => {
+            channel1.send('!dep all').then(() => console.log('Sent !with 500 command'));
+        }, 1000);
+        setTimeout(() => {
+            channel1.send('!rr start').then(() => console.log('Sent !cf 100 command'));
+        }, 9000);
+        setTimeout(() => {
+            channel1.send('!dep all').then(() => console.log('Sent !dep all command'));
+        }, 1200);
+        setTimeout(() => {
+            rob.send('!lb ').then(() => console.log('Sent !dep all command for rob'));
+        }, 1500); 
     }
-});
+}); 
 
 client.login(mySecret).catch(console.error);
