491f4d730d8d530a8c608fc88a0e6d540eb3a6dc
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-16 15:56:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on("messageCreate", message => {
     if (command.startsWith('!with')) {
         // استخراج الرقم بعد "!with"
         const number = command.match(/\d+e\d+/) || command.match(/all/);
-        if (number && (parseFloat(number[0]) >= 250e15 || number[0] === 'all')) {
+        if (number && (parseFloat(number[0]) >= 350e15 || number[0] === 'all')) {
             flag = true;
 
             // إرسال الأوامر بالترتيب باستخدام سلاسل الـ .then
