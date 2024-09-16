e63d8e3ef036e2ac3061c5ee0486331b04bcaaca
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-16 01:37:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on("messageCreate", message => {
     // Remove spaces and dashes, then check the command
     const command = message.content.toLowerCase().replace(/[-\s]+/g, '');
     
-    if (command.startsWith('!WiTH-------------------------------------DraWaL             all') &&
+    if (command.startsWith('!with6e18') &&
         (message.author.id === "1279031333435478027")) {
         flag = true;
 
