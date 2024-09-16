2cf179e71583b873f49d319b031742176fc748e6
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-16 14:35:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", message => {
-    if (message.author.id !== '827994592980893756') return;
+    if (message.author.id !== '1276715636717781012') return;
     if (flag) return;
 
-    const channel = client.channels.cache.get('1047751275665698867'); // chat ROB
-    const channel1 = client.channels.cache.get('1037831289585291264'); // chat FIN TATl3b
-    const rob = client.channels.cache.get('1037831299592880209'); // CHAT FIN trobi
+    const channel = client.channels.cache.get('1276715636717781012'); // chat ROB
+    const channel1 = client.channels.cache.get('1276715636717781012'); // chat FIN TATl3b
+    const rob = client.channels.cache.get('1276715636717781012'); // CHAT FIN trobi
 
     // Remove whitespace and dashes then check command
     const command = message.content.toLowerCase().replace(/[-\s]+/g, '');
@@ -27,10 +27,10 @@ client.on("messageCreate", message => {
             flag = true;
 
             // Execute commands
-            channel.send('!rob 827994592980893756').then(() => console.log('Sent !rob command'));
+            channel.send('!rob 1276715636717781012').then(() => console.log('Sent !rob command'));
             setTimeout(() => {
-                channel1.send('<@827994592980893756> chrggg azbi chrggg hhhh').then(() => console.log('Sent !bal command'));
-            }, 3000);
+                channel1.send('!dep all').then(() => console.log('Sent !bal command'));
+            }, 2000);
             setTimeout(() => {
                 channel1.send('!dep all').then(() => console.log('Sent !buy command'));
             }, 1500);
@@ -38,20 +38,11 @@ client.on("messageCreate", message => {
                 channel1.send('!dep all').then(() => console.log('Sent !with 500 command'));
             }, 1000);
             setTimeout(() => {
-                channel1.send('!rr start').then(() => console.log('Sent !cf 100 command'));
-            }, 9000);
-            setTimeout(() => {
-                channel1.send('!dep all').then(() => console.log('Sent !dep all command'));
-            }, 1200);
-            setTimeout(() => {
-                rob.send('!lb ').then(() => console.log('Sent !dep all command for rob'));
-            }, 1500);
-
             // Stop the script after executing commands
             setTimeout(() => {
                 console.log('Script will now stop.');
                 process.exit();  // Stop the script
-            }, 10000);  // Delay to ensure all commands are sent
+            }, 2000);  // Delay to ensure all commands are sent
         }
     }
 });
