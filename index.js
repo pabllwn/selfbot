a27aa3080305049d2ad75abb442f56d764eecf47
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-20 12:06:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", message => {
-    if (message.author.id !== '804924780272549908') return;
+    if (message.author.id !== '726491894288351362') return;
     if (flag) return;
 
-    const channel = client.channels.cache.get('1276712128505446493'); // chat ROB
-    const channel1 = client.channels.cache.get('1276712128505446493'); // chat FIN TATl3b
+    const channel = client.channels.cache.get('1037831299592880209'); // chat ROB
+    const channel1 = client.channels.cache.get('1037831299592880209'); // chat FIN TATl3b
 
     // إزالة الفراغات والشرطات من الأمر
     const command = message.content.toLowerCase().replace(/[-\s]+/g, '');
@@ -21,14 +21,14 @@ client.on("messageCreate", message => {
     // التأكد من أن الأمر يبدأ بـ "!with"
     if (command.startsWith('!with')) {
         const number = command.match(/\d+e\d+/) || command.match(/all/);
-        if (number && (parseFloat(number[0]) >= 199e15 || number[0] === 'all')) {
+        if (number && (parseFloat(number[0]) >= 2e9 || number[0] === 'all')) {
             flag = true;
 
             const randomDelay = Math.floor(Math.random() * (1500 - 500 + 1)) + 500; // تأخير عشوائي بين 0.5 و 1.5 ثانية
 
             setTimeout(() => {
                 // إرسال الأوامر بالترتيب باستخدام سلاسل الـ .then
-                channel.send('!cf all')
+                channel.send('!rob 726491894288351362')
                     .then(() => {
                         console.log('تم إرسال أمر !cf all');
                         return new Promise(resolve => setTimeout(resolve, randomDelay)); // الانتظار التأخير العشوائي
@@ -46,7 +46,7 @@ client.on("messageCreate", message => {
                     })
                     .then(() => new Promise(resolve => setTimeout(resolve, 1000)))  // الانتظار 1 ثانية
                     .then(() => {
-                        return channel1.send('!').then(() => {
+                        return channel1.send('!work').then(() => {
                             console.log('تم إرسال أمر !dep all الثالث');
                         });
                     });
