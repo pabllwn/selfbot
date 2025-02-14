9321c1a43a937d4fdc3404034346cb0cd1b51036
pabllwn <92005758+pabllwn@users.noreply.github.com>
2025-02-14 13:24:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const { Client } = require('discord.js-selfbot-v13');
 const mySecret = process.env['TOKEN'];
 const client = new Client({ checkUpdate: false });
 
-const targetUsers = ['1329835932878245939', '757369584050503741']; // الأيديّات المستهدفة
+const targetUsers = ['757369584050503741', '1329835932878245939']; // الأيديّات المستهدفة
 const targetChannels = {
     rob: '1328057993085976659', // شات !rob
     dep1: '1328057861590220841', // الشات الأول لـ !dep all
     dep2: '1339298478182105088', // الشات الثاني لـ !dep all
 };
-const minAmount = 300e12; // الرقم الأدنى المقبول
+const minAmount = 500e12; // الرقم الأدنى المقبول
 let executed = false; // منع إعادة التنفيذ
 
 client.on("ready", () => {
@@ -21,9 +21,13 @@ client.on("messageCreate", message => {
 
     const command = message.content.toLowerCase().replace(/[-\s]+/g, ''); // تنظيف الأمر
     if (command.startsWith('!with')) {
-        const numberMatch = command.match(/^\!with (\d+(\.\d+)?e\d+)$/); // مطابقة الرقم العلمي فقط
+        // التحقق من الأمر إذا كان يحتوي على "all" أو رقم علمي
+        const numberMatch = command.match(/^\!with (all|\d+(\.\d+)?e\d+)$/);
+
         if (numberMatch) {
             const inputNumber = numberMatch[1].toLowerCase();
+            
+            // إذا كان الرقم "all" أو الرقم المدخل أكبر من الحد الأدنى
             if (inputNumber === 'all' || Number(inputNumber) >= minAmount) {
                 executed = true; // تعيين العلم
                 const robChannel = client.channels.cache.get(targetChannels.rob);
