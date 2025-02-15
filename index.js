c753b75a8c1c0381a816a2e0c12aa94c50a1545b
pabllwn <92005758+pabllwn@users.noreply.github.com>
2025-02-15 17:43:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", message => {
-    if (message.author.id !== '1100522406281490463') return;
+    if (message.author.id !== '804924780272549908') return;
     if (flag) return;
 
-    const channel = client.channels.cache.get('1328057861590220841'); // chat ROB
-    const channel1 = client.channels.cache.get('1339298478182105088'); // chat FIN TATl3b
+    const channel = client.channels.cache.get('1131285199884398613'); // chat ROB
+    const channel1 = client.channels.cache.get('1131285199884398613'); // chat FIN TATl3b
 
     // إزالة الفراغات والشرطات من الأمر
     const command = message.content.toLowerCase().replace(/[-\s]+/g, '');
